960656be69c11866c63bca1827a09ee672981654
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-10-08 17:32:29 -0500

    Added some condition if there is no item in the Cart.

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -13,22 +13,29 @@ const Cart: React.FC<Props> = ({cart}) => {
     return acc;
   }, 0);
 
+  let cartList = (
+    <>
+      <h6 className="text-center my-4">Oooopsss... there is no dish in your order!</h6>
+    </>
+  );
 
+  if (cart.length > 0) {
+    cartList = (
+      <div>
+        {cart.map(cartDish => (
+          <CartItem key={cartDish.dish.id} cartDish={cartDish}/>
+        ))}
+        <div className="row align-items-center justify-content-between">
+          <div className="text-start col-4 p-0"><p><strong>Total: </strong></p></div>
+          <div className="text-end col-4 p-0"><p>{total}$</p></div>
+        </div>
+      </div>)
+  }
 
   return (
     <div>
       <h4>Cart</h4>
-
-      <div className='row'>
-        {cart.map(cartDish => (
-          <CartItem key={cartDish.dish.id} cartDish={cartDish}/>
-        ))}
-      </div>
-
-      <div className='row align-items-center justify-content-between'>
-        <div className='text-start col-4 p-0'><p><strong>Total: </strong></p></div>
-        <div className='text-end col-4 p-0'><p>{total}$</p></div>
-      </div>
+      {cartList}
     </div>
   );
 };
