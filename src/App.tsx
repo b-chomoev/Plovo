3e2771c9dbd1c67456e538f15586760653f81f24
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-11-15 16:15:35 -0600

    Fetching data from the server is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,25 @@
 import { useCallback, useEffect, useState } from 'react';
-import { DishesList, IDish } from './types';
 import Home from './containers/Home/Home';
 import NewDish from './containers/NewDish/NewDish';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import CheckOut from './containers/CheckOut/CheckOut';
 import Order from './containers/Order/Order';
-import axiosAPI from './axiosAPI';
 import EditDish from './containers/EditDish/EditDish';
 import Orders from './containers/Orders/Orders';
 import Layout from './components/Layout/Layout';
-import { useAppDispatch } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 import { updateCart } from './store/slices/cartSlice';
+import { fetchAllDishes } from './store/thunks/dishesThunk';
+import { selectDishes } from './store/slices/dishesSlice';
 
 const App = () => {
-  const [dishes, setDishes] = useState<IDish[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const location = useLocation();
+  const dishes = useAppSelector(selectDishes);
   const dispatch = useAppDispatch();
 
   const fetchDishes = useCallback(async () => {
-    try {
-      setLoading(true);
-      const responseDishes: {data: DishesList | null} = await axiosAPI('dishes.json');
-      const dishesList = responseDishes.data;
-
-      if (dishesList === null) {
-        setDishes([]);
-        return;
-      }
-
-      const dishes: DishesList = dishesList;
-
-      const dishesInMyFormat = Object.keys(dishesList).map(dish => {
-        return {
-          ...dishes[dish],
-          id: dish,
-        };
-      });
-
-      setDishes(dishesInMyFormat);
-      dispatch(updateCart(dishesInMyFormat));
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setLoading(false);
-    }
+    await dispatch(fetchAllDishes());
   }, [dispatch]);
 
   useEffect(() => {
@@ -52,6 +27,10 @@ const App = () => {
       void fetchDishes();
     }
   }, [fetchDishes, location]);
+
+  useEffect(() => {
+    dispatch(updateCart(dishes));
+  }, [dishes, dispatch]);
 
   return (
     <>
--- src/store/slices/dishesSlice.ts
+++ src/store/slices/dishesSlice.ts
@@ -1,5 +1,7 @@
 import { IDish } from '../../types';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchAllDishes } from '../thunks/dishesThunk';
+import { RootState } from '../../app/store';
 
 interface DishState {
   dishes: IDish[];
@@ -11,11 +13,24 @@ const initialState: DishState = {
   isFetchingLoading: false,
 };
 
+export const selectDishes = (state: RootState) => state.dishes.dishes;
+
 export const dishesSlice = createSlice({
   name: 'dishes',
   initialState,
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllDishes.pending, state => {
+        state.isFetchingLoading = true;
+      })
+      .addCase(fetchAllDishes.fulfilled, (state, action: PayloadAction<IDish[]>) => {
+        state.dishes = action.payload;
+        state.isFetchingLoading = true;
+      })
+      .addCase(fetchAllDishes.rejected, state => {
+        state.isFetchingLoading = false;
+      });
   }
 });
 
